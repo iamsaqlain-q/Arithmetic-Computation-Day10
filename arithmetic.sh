20d46b582c737ad9e933d72fe34814dd7dc78786
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-05 17:08:21 +0530

    compute a*b+c

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,8 +1,33 @@
-echo "Enter a"
+#!/bin/bash -x
+
+
+#Welcome to Arithmetic Computation Program on Master #Branch
+
+
+echo ""
+echo "Welcome to the Arithmetic Computation and Sorting Program"
+echo "This program computes different arithmetic expressions and sorts the results."
+echo ""
+
+#Write program to take three inputs – a, b & c
+echo -n " Enter the first number : "
 read num1
-echo "Enter b"
+echo -n " Enter the second number : "
 read num2
-echo "Enter c"
+echo -n " Enter the third number : "
 read num3
 
-c=`echo  $num1 $num2 $num3 | awk  '{print $num1+$num2*$num3}'` 
+echo "You entered $num1 $num2 $num3"
+echo ""
+
+#Compute a + b * c
+operationum1=$((num1+$((num2*$num3))))
+
+echo "Computation 1 :     a + b * c = $operationum1"
+echo ""
+
+#Compute a * b + c  
+operationum2=$(($((num1*$num2))+$num3))
+
+echo "Computation 2 :     a * b + c = $operationum2"
+echo ""
