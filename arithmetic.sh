210638745f26caef02819bf24b7bb2a01b52b6b1
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-05 17:25:26 +0530

    Read values dictionary to array

--- arithmetic.sh
+++ arithmetic.sh
@@ -12,50 +12,62 @@ echo ""
 
 #Write program to take three inputs – a, b & c
 echo -n " Enter the first number : "
-read num1
+read n1
 echo -n " Enter the second number : "
-read num2
+read n2
 echo -n " Enter the third number : "
-read num3
+read n3
 
-echo "You entered $num1 $num2 $num3"
+echo "You entered $n1 $n2 $n3"
 echo ""
 
 
 #Compute a + b * c
-operationum1=$((num1+$((num2*$num3))))
+operation1=$((n1+$((n2*$n3))))
 
-echo "Computation 1 :     a + b * c = $operationum1"
+echo "Computation 1 :     a + b * c = $operation1"
 echo ""
 
 
 #Compute a * b + c  
-operationum2=$(($((num1*$num2))+$num3))
+operation2=$(($((n1*$n2))+$n3))
 
-echo "Computation 2 :     a * b + c = $operationum2"
+echo "Computation 2 :     a * b + c = $operation2"
 echo ""
 
 
 #Compute c + a / b
-operationum3=$((num3+$((num1/$num2))))
+operation3=$((n3+$((n1/$n2))))
 
-echo "Computation 3 :     c + a / b = $operationum3"
+echo "Computation 3 :     c + a / b = $operation3"
 echo ""
 
 
 #Compute a % b + c
-operation4=$(($((num1%$num2))+$num3))
+operation4=$(($((n1%$n2))+$n3))
 
 echo "Computation 4 :     a % b + c = $operation4"
 echo ""
 
 
-#Store the results in a Dictionary
-result=([1]=$operationum1 [2]=$operationum2 [3]=$operationum3 [4]=$operation4) 
+#Store the results in a Dictionary for every Computation
+result=([1]=$operation1 [2]=$operation2 [3]=$operation3 [4]=$operation4) 
 echo "Displaying Dictionary : "
 echo "Operation Key Value"
 for key in "${!result[@]}"; do
     echo "Computation $((key+1))   ${result[$key]}"
 done
 echo ""
+
+#Read the values from the Dictionary into the array
+declare -a arrayResult
+for key in "${!result[@]}"; do
+	arrayResult[$key]=${result[$key]}
+done
+echo ""
+
+echo "Array ${arrayResult[@]} "
+echo ""
+
+
 
